3b63f725bedee7c103cea30b46c10852826192fc
AidanBrasseur <aidanbrasseur@gmail.com>
2021-02-28 11:31:57 -0500

    Update craco.config.js

--- fitmeals/craco.config.js
+++ fitmeals/craco.config.js
@@ -7,10 +7,12 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': '#AAA713',
+            modifyVars: { '@primary-color': '#032D23',
              '@btn-primary-bg': '#032D23', 
              '@btn-border-radius-base': '10px',
-            '@btn-border-radius-sm': '10px',
+            '@btn-border-radius-sm': '10px', 
+            '@checkbox-color': '#AAA713',
+            '@link-color': '#AAA713',
         },
             javascriptEnabled: true,
           },
