9e3236471b96988e8ad0e13b30481a0b175f7e46
AidanBrasseur <aidanbrasseur@gmail.com>
2021-02-28 00:03:02 -0500

    modifying login page. Fixing carousel
    will need to do the same for the registration

--- fitmeals/src/pages/LoginPage/LoginPage.tsx
+++ fitmeals/src/pages/LoginPage/LoginPage.tsx
@@ -1,63 +1,97 @@
-import { Button, Layout, Input, Image, Typography, Carousel } from 'antd';
+import Icon, { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { Button, Carousel, Checkbox, Col, Form, Image, Input, Layout, Row, Space, Typography } from 'antd';
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom';
+import { ReactComponent as logoSVG } from '../../assets/logo.svg';
 import './styles.css';
 
-function login (){
-    
-}
 
-function LoginPage () {
-
+function LoginPage() {
+    const history = useHistory();
+    function login() {
+        history.push("/");
+    }
     return (
         <Layout>
-            <Layout.Content style={{ height: "100vh" }}>
-                <div className="loginDiv"> 
-                    <div className="logo">
-                        <Image 
-                            src="https://cdn.iconscout.com/icon/free/png-256/fast-food-1851561-1569286.png"
-                            width="75%"
-                            height="75%"
-                            preview={ false }
-                        ></Image>
-                    </div>
-                    <div className="loginForm">
-                        <Typography.Title style={{ fontSize: 36 }}>Welcome to <span className="name">FitMeals</span></Typography.Title>
-                        <Typography.Title style={{ fontSize: 60, color: "#032D23" }}>Login</Typography.Title>
-                        <div className="mainLoginForm">
-                            <Input size="large" style={{ marginTop: 20, marginBottom: 10, padding: 10, borderRadius: 8, borderColor: "#032D23" }} placeholder="Email"></Input>
-                            <Input.Password size="large" style={{ marginBottom: 5, padding: 10, borderRadius: 8, borderColor: "#032D23" }} placeholder="Password"></Input.Password>
-                            <Button type="link" style={{ color: "#032D23", fontSize: 15, padding: 0, float: "right" }}>Forget Password?</Button>
-                            <Button 
-                                type="primary" 
-                                style={{ marginTop: 25, marginBottom: 20, width: "100%", fontSize: 18, height: 50, borderRadius: 8, backgroundColor: "#032D23", border: 0 }}
-                                onClick={() => { login() }}
-                            >Login</Button>
-                            <div className="registerButton">
-                                Don't have an account?<Link to="/register" style={{ color: "#AAA713", marginLeft: 3, padding: 0 }}>Get Started</Link>
-                            </div>
+            <Layout.Content style={{ height: '100vh' }}>
+                <Row justify='center' className='loginDiv'>
+                    <Col span={12} >
+
+                        <div className='logo'>
+                            <Icon component={logoSVG} style={{ fontSize: 125 }} />
                         </div>
-                    </div>
-                    <div className="TaCDiv">
-                        <Button type="link" style={{ color: "#032D23", fontSize: 15 }}>Terms and Conditions</Button>
-                    </div>
-                </div> 
-                <Carousel className="imageDiv">
-                    <div>
-                        <Image 
-                            src="https://media1.popsugar-assets.com/files/thumbor/q_eu4G_Yfvd1qUU7rkJYpC9Qalk/0x532:1560x2092/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/11/18/102/n/1922729/2010a3325dd3450317e273.27544324_/i/healthy-meal-prep-dinner-recipes.jpg"
-                            preview={false}
-                        ></Image>
-                    </div>
-                    <div>
-                        <Image 
-                            src="https://media1.popsugar-assets.com/files/thumbor/q_eu4G_Yfvd1qUU7rkJYpC9Qalk/0x532:1560x2092/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/11/18/102/n/1922729/2010a3325dd3450317e273.27544324_/i/healthy-meal-prep-dinner-recipes.jpg"
-                            preview={false}
-                        ></Image>
-                    </div>
-                </Carousel>             
-            </Layout.Content>         
-        </Layout>       
+                        <div className='centerDiv'>
+                            <Space direction='vertical' style={{ width: '50%', marginRight: 'auto', marginLeft: 'auto' }}>
+                                <Typography.Title style={{ fontSize: 36 }}>Welcome to <span className="name">FitMeals</span></Typography.Title>
+                                <Typography.Title style={{ fontSize: 60, color: "#032D23" }}>Login</Typography.Title>
+                                <Form
+                                    name="normal_login"
+                                    className="login-form"
+
+                                    initialValues={{ remember: true }}
+                                    onFinish={() => console.log('finished')}
+                                >
+                                    <Form.Item
+                                        name="email"
+                                        rules={[{ required: true, message: 'Please input your email!' }]}
+                                    >
+                                        <Input prefix={<MailOutlined className="site-form-item-icon" />} placeholder="Email" />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name="password"
+                                        rules={[{ required: true, message: 'Please input your password!' }]}
+                                    >
+                                        <Input.Password
+                                            prefix={<LockOutlined className="site-form-item-icon" />}
+                                            type="password"
+                                            placeholder="Password"
+                                        />
+                                    </Form.Item>
+                                    <Form.Item>
+                                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                                            <Checkbox>Remember me</Checkbox>
+                                        </Form.Item>
+
+                                        <a className="login-form-forgot" href="">
+                                            Forgot password?</a>
+                                    </Form.Item>
+                                    <Form.Item >
+                                        <Button type="primary" style={{ backgroundColor: '#032D23', borderRadius: 8 }} onClick={login}>Login</Button>
+                                        <div className="registerButton">
+                                            Don't have an account?<Link to="/register" style={{ marginLeft: 3, padding: 0 }}>Get Started</Link>
+                                        </div>
+                                    </Form.Item>
+                                </Form>
+                            </Space>
+                        </div>
+
+
+                    </Col>
+                    <Col span={12} style={{ height: '100%', width: '100%' }}>
+
+                        <Carousel swipeToSlide draggable adaptiveHeight={false} variableWidth={false} >
+
+                            <Image
+                                src="https://media1.popsugar-assets.com/files/thumbor/q_eu4G_Yfvd1qUU7rkJYpC9Qalk/0x532:1560x2092/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/11/18/102/n/1922729/2010a3325dd3450317e273.27544324_/i/healthy-meal-prep-dinner-recipes.jpg"
+                                preview={false}
+                                style={{ objectFit: 'cover' }}
+
+                            ></Image>
+
+
+                            <Image
+                                src="https://media1.popsugar-assets.com/files/thumbor/q_eu4G_Yfvd1qUU7rkJYpC9Qalk/0x532:1560x2092/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/11/18/102/n/1922729/2010a3325dd3450317e273.27544324_/i/healthy-meal-prep-dinner-recipes.jpg"
+                                preview={false}
+                                style={{ objectFit: 'cover' }}
+                            ></Image>
+
+                        </Carousel>
+
+                    </Col>
+
+                </Row>
+            </Layout.Content>
+        </Layout>
     );
 
 }
