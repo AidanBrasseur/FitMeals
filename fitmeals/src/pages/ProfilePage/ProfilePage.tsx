1b9f1ef278e390c2db9ba41035a2f844f52abe6b
SaeyonSivakumaran <saeyon2000@gmail.com>
2021-03-05 14:05:51 -0500

    Add fake recipes to profile page

--- fitmeals/src/pages/ProfilePage/ProfilePage.tsx
+++ fitmeals/src/pages/ProfilePage/ProfilePage.tsx
@@ -2,6 +2,7 @@ import { UserOutlined, StarOutlined } from '@ant-design/icons';
 import { Avatar, Button, Col, Form, Input, Layout, Row, Select, Typography, Upload } from 'antd';
 import React from 'react';
 import Header from '../../components/Header/Header';
+import ProfileFeed from '../../components/ProfileFeed/ProfileFeed'
 
 import './styles.css';
 
@@ -20,11 +21,15 @@ function ProfilePage() {
           <div className="profileInfo">
             <p className="name">Rowan Atkinson</p>
             <p className="username">mrbean55</p>
-            <div className="rating">
+            <div className="userRating">
               <Row style={{ paddingTop: 5, paddingBottom: 5, paddingLeft: 5, paddingRight: 5 }} justify='space-around' align='middle'>
                 <StarOutlined style={{ color: 'white', fontSize: 20 }} />
                 <div style={{ color: 'white' }}>Average rating of 4.5</div>
               </Row>
+            </div>
+            <div className="recipes">
+              <p className="recipeTitle">Recent Recipes</p>
+              <ProfileFeed></ProfileFeed>
             </div>
           </div>
         </div>
