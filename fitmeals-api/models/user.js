bf34055fec1c3ee46c0f039a4cc9105068945d96
SaeyonSivakumaran <saeyon2000@gmail.com>
2021-03-27 16:00:39 -0400

    Add basic GET and POST users API for testing MongoDB connection

---
+++ fitmeals-api/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    authToken: String,
+    fullname: String,
+    email: String,
+    profileImageURL: String,
+    isAdmin: Boolean
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
--- fitmeals-api/server.js
+++ fitmeals-api/server.js
@@ -1,9 +1,64 @@
 // Creating the Express app
 const express = require('express')
 const app = express();
+app.use(express.json());
 
 // Get the Mongoose instance
 const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+
+/*
+GET: /users
+Get all the users of FitMeals
+*/
+app.get("/users", (req, res) => {
+	// Check for a valid mongoose connection
+	if (mongoose.connection.readyState != 1) {
+		log('Issue with mongoose connection');
+		res.status(500).send('Internal server error');
+		return;
+	}
+
+	// Return all the users
+	User.find().then((users) => {
+		res.send({ users: users });
+	}).catch((error) => {
+		log(error);
+		res.status(500).send("Internal Server Error");
+	});
+});
+
+/*
+POST: /users
+Adds a new user to the FitMeals DB
+*/
+app.post("/users", (req, res) => {
+	// Check for a valid mongoose connection
+	if (mongoose.connection.readyState != 1) {
+		log('Issue with mongoose connection');
+		res.status(500).send('Internal server error');
+		return;
+	}
+
+	// Create a new user to be posted to the DB
+	let user = new User({
+		username: req.body.username,
+		password: req.body.password,
+		authToken: "TEMP_AUTH_TOKEN",
+		fullname: req.body.fullname,
+		email: req.body.email,
+		profileImageURL: "TEMP_PROFILE_IMAGE_URL",
+		isAdmin: false
+	}); 
+
+	// Saving the user to the DB
+	user.save().then((result) => {
+		res.send(result);
+	}).catch((error) => {
+		console.log(error);
+		res.status(500).send("Internal Server Error");
+	});
+});
 
 // Running the Express app
 const port = process.env.PORT || 5000
