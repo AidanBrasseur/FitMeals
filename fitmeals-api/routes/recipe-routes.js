aedcaec65e121e2d9139a7ca3336455afa79864a
Priyanshu Arora <priyarora4@gmail.com>
2021-03-30 21:48:47 -0400

    post recipe route api

--- fitmeals-api/routes/recipe-routes.js
+++ fitmeals-api/routes/recipe-routes.js
@@ -5,6 +5,18 @@ const { mongoose } = require('../db/mongoose');
 const { Recipe } = require('../models/recipe');
 const { User } = require('../models/user');
 const { ObjectID } = require('mongodb')
+const upload = require('../middleware/upload');
+// const  multer  = require('multer')
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb){
+//         cb(null, )
+//     }, 
+//     filename: function(req, file, cb){
+
+//     }
+// });
+
+// const upload = multer({ dest: 'uploads/' });
 /*
 Get: /recipes
 Gets currently approved recipe preview 
@@ -101,5 +113,81 @@ router.get("/users/:id", async (req, res) => {
         return;
     }
 })
+
+
+router.post('/', async (req, res) => {
+    // Add code here
+    // check mongoose connection established.
+
+    try {
+        await upload(req, res)
+    } catch(error){
+        console.log(error)
+        res.send('Error when trying to upload image');
+        return;
+    }
+    
+    if (mongoose.connection.readyState != 1) {
+        console.log('Issue with mongoose connection')
+        res.status(500).send('Internal server error')
+        return;
+    }
+    console.log(req.file);
+
+    // Create a new restaurant using the restaurant mongoose model
+    // const restaurant = new Restaurant({
+    // 	name: req.body.name,
+    // 	description: req.body.description,
+    // 	reservations: []
+    // })
+
+    const user = await User.findOne({ authToken: req.headers.authorization });
+    if (!user) {
+        res.status(401).send({ success: false, error: "Unauthorized" });
+        return;
+    }
+
+
+    console.log(user);
+    console.log(JSON.parse(req.body.instructions))
+
+    const recipe = new Recipe({
+        user: user._id,
+        rating: req.body.rating,
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        description: req.body.description,
+        time: req.body.time,
+        calories: req.body.calories,
+        image: req.file.id,
+        approved: false,
+        date: Date.now(),
+        ingredients: req.body.ingredients ? JSON.parse(req.body.ingredients) : [],
+        categories: req.body.categories ? JSON.parse(req.body.categories) : [],
+        instructions: req.body.instructions ? JSON.parse(req.body.instructions) : [],
+        comments: [],
+        macros: req.body.macros
+    })
+
+    // Save restuarant to the database
+    try {
+    	const result = await recipe.save()
+    	res.send(recipe)
+    } catch (error) {
+    	console.log(error) // log server error to the console, not to the client.
+    	if (isMongoError(error)) { // check for if mongo server suddenly dissconnected before this request.
+    		res.status(500).send('Internal server error')
+    	} else {
+    		res.status(400).send('Bad Request') // 400 for bad request gets sent to client.
+    	}
+    }
+
+})
+
+function isMongoError(error) { // checks for first error returned by promise rejection if Mongo database suddently disconnects
+	return typeof error === 'object' && error !== null && error.name === "MongoNetworkError"
+}
+
+
 
 module.exports = router;
