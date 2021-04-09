98952136cec1ae7cad4dccc0f8cf4a9329f6b4c1
SaeyonSivakumaran <saeyon2000@gmail.com>
2021-04-08 23:24:26 -0400

    Add redirect to Postman docs

--- fitmeals-api/server.js
+++ fitmeals-api/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 // API routes
 app.use("/auth", require('./routes/auth-routes'));
 app.use("/admin", require('./routes/admin-routes'));
@@ -11,6 +12,21 @@ app.use("/recipes", require('./routes/recipe-routes'));
 app.use("/comments", require('./routes/comment-routes'));
 app.use("/users", require('./routes/users-routes'));
 
+/*
+GET: "/"
+Redirect to the API docs
+*/
+app.get("/", (req, res) => {
+	res.redirect("/docs");
+})
+
+/*
+GET: "/docs"
+Redirect to the Postman API docs
+*/
+app.get("/docs", (req, res) => {
+	res.redirect("https://documenter.getpostman.com/view/15303556/TzCTa5pU");
+})
 
 // Running the Express app
 const port = process.env.PORT || 5000
